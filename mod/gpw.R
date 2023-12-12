f8f8758c4007d0ba0ea5c2d23d8102da8d2af0fd
gfigon <grzegorz.figon@gmail.com>
2023-12-12 12:32:34 +0100

    Added functions for getting and cleaning data. Added and tested R script for downloading, cleaning and merge income statements.

--- mod/gpw.R
+++ mod/gpw.R
@@ -4,8 +4,14 @@ box::use(httr[...],
          zoo[...],
          rvest[...],
          dplyr[...],
+         tidyr = tidyr[separate],
          purrr = purrr[map, slowly, rate_delay],
-         readr = readr[read_csv]
+         readr = readr[read_csv],
+         janitor = janitor[clean_names],
+         sjmisc = sjmisc[rotate_df],
+         stats = stats[na.omit],
+         stringr = stringr[str_remove_all],
+         readr = readr[parse_number]
 )
 
 
@@ -84,4 +90,67 @@ get_table <- function(table_url){
     html_table()
   
 }
+
+
+#' @export
+get_companies <- function(table_url){
+  
+  table_path <- '//*[@id="right-content"]/div/table'
+  
+  set_config(add_headers(`User-Agent` = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"))
+  
+  get_delayed <- slowly(~ GET(.), 
+                        rate = rate_delay(3))
+  
+  get_result <- get_delayed(table_url)
+  
+  raw_html <- read_html(get_result)
+  
+  raw_html %>% html_nodes(xpath = table_path) %>% 
+    html_table()
+  
+}
+
+#' @export
+get_companies_links <- function(table_url){
+  
+  table_path <- '//*[@id="right-content"]/div/table'
+  
+  set_config(add_headers(`User-Agent` = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"))
+  
+  get_delayed <- slowly(~ GET(.), 
+                        rate = rate_delay(3))
+  
+  get_result <- get_delayed(table_url)
+  
+  raw_html <- read_html(get_result)
+  
+  companies <- raw_html |> html_elements(xpath = table_path) |> html_table()
+  companies <- companies[[1]] |> clean_names() |>
+    separate(col = profil, into = c("ticker", "name"), sep = " ") |> select(ticker) |> 
+    filter(nchar(ticker) == 3)
+  
+  c_links <- tibble(link = paste0("https://www.biznesradar.pl", 
+                                  raw_html |> html_elements(xpath = table_path)  |> 
+                      html_elements(".bvalue") |> html_elements("a") |> html_attr("href"),
+           ",", "Q"))
+  
+  c_links <- c_links |> filter(link != "https://www.biznesradar.pl/raporty-finansowe-rachunek-zyskow-i-strat/SVRS,Q")
+
+  companies$link <- c_links$link
+  companies
+  
+}
+
+
+#' @export
+clean_rzis <- function(tb){
+  tb |> clean_names() |> rotate_df(cn = TRUE) |> as_tibble() |> clean_names() |> 
+    mutate(data_publikacji = as.Date(data_publikacji)) |>
+    mutate(across(przychody_ze_sprzedazy:ebitda, str_remove_all, " ")) |> 
+    mutate(across(przychody_ze_sprzedazy:ebitda, parse_number)) |> na.omit() 
+}
+
+
+
 
